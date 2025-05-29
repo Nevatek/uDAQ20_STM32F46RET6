7e0c6985bfd70e3b413ae045128dac839ebb1ed5
Aldrin Rebellow <aldrinrebellow7@gmail.com>
2025-05-29 15:51:19 +0530

    update

--- CODE/uDAQ20_STM32F446RET6/Core/Src/Drv_AD7616.c
+++ CODE/uDAQ20_STM32F446RET6/Core/Src/Drv_AD7616.c
@@ -95,7 +95,7 @@ void Drv_AD7616_Init(void)
 	HAL_GPIO_WritePin(AD7616_RESET_GPIO_Port , AD7616_RESET_Pin , GPIO_PIN_SET);/*Make RESET pin HIGH*/
 	HAL_GPIO_WritePin(AD7616_CS__GPIO_Port , AD7616_CS__Pin , GPIO_PIN_SET);/*Make CS pin HIGH*/
 	Drv_AD7616_SelectChannel(m_MaxChannelScan = AD7616_CHAB7/*MAX Channel to scan*/);/*Sequencer will read from channel 0 - Configured channel*/
-	Drv_AD7616_SelectHWInputVoltageRange(RANGE_SEL_PM_2_5V);/*Select HARDWARE MODE and INP voltage range*/
+	Drv_AD7616_SelectHWInputVoltageRange(RANGE_SEL_PM_5V);/*Select HARDWARE MODE and INP voltage range*/
 	HAL_GPIO_WritePin(AD7616_CONV_GPIO_Port , AD7616_CONV_Pin , GPIO_PIN_RESET);/*Make CONV pin RESET*/
 
 	HAL_Delay(TIME_RESET_WAIT);
@@ -405,11 +405,14 @@ void Drv_AD7616_Handler(void)
 					u16BuffChB[u8Channel] = (pBuff[((u8Index + 2U)) + 0U]);
 					u16BuffChB[u8Channel] |= (pBuff[((u8Index + 2U)) + 1U] << 8U);
 #else
-					g_u16ADCDataVal_ChannelA[u8Channel] = (pBuff[(u8Index) + 0U]);
-					g_u16ADCDataVal_ChannelA[u8Channel] |= (pBuff[(u8Index) + 1U] << 8U);
+					g_u16ADCDataVal_ChannelA[u8Channel] = 0U;
+					g_u16ADCDataVal_ChannelB[u8Channel] = 0U;
+					g_u16ADCDataVal_ChannelA[u8Channel] = 0xFF00 & (pBuff[(u8Index) + 1U] << 8U);
+					g_u16ADCDataVal_ChannelA[u8Channel] |= 0x00FF & (pBuff[(u8Index) + 0U]);
 
-					g_u16ADCDataVal_ChannelB[u8Channel] = (pBuff[((u8Index + 2U)) + 0U]);
-					g_u16ADCDataVal_ChannelB[u8Channel] |= (pBuff[((u8Index + 2U)) + 1U] << 8U);
+
+					g_u16ADCDataVal_ChannelB[u8Channel] = (pBuff[((u8Index + 2U)) + 1U] << 8U);
+					g_u16ADCDataVal_ChannelB[u8Channel] |= (pBuff[((u8Index + 2U)) + 0U]);
 #endif
 #if (TRUE)
 					/*
